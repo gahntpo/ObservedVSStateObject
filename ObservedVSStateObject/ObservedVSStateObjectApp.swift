9fa0c0c2cbc8081cdaee9567d898d03f0aa130dc
Karin Prater <karin.prater@gmx.de>
2021-01-10 10:10:27 +0200

    Initial Commit

---
+++ ObservedVSStateObject/ObservedVSStateObjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  ObservedVSStateObjectApp.swift
+//  ObservedVSStateObject
+//
+//  Created by Karin Prater on 10.01.21.
+//
+
+import SwiftUI
+
+@main
+struct ObservedVSStateObjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
